02292fa0c01b96f7b73576744ea6edefe63dde9e
samirashaine <samira.shaine@hotmail.com>
2021-02-01 19:17:54 -0500

    Update script.js
    updated ajax mechanism for it to be dynamic

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ function loadpage(e){
     $("#menu a.active").removeClass("active");
     $(this).addClass("active");
 
-    $("#content").load("about.html");
+
+    var href = $(this).attr("href");
+    $("#content").load(href);
 }
 
 
